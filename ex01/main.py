1645cc582cca32b78cd5f4cda03d5b8412d74c8d
barami62 <wodbs7758@naver.com>
2024-05-20 12:29:13 +0900

    todo router 추가

--- ex01/main.py
+++ ex01/main.py
@@ -1,13 +1,18 @@
 from fastapi import FastAPI
 
+from todo import todo_router
+
 app = FastAPI()
 
 
 @app.get("/")
 async def root():
-    return {"message": "Hello World"}
+  return {"message": "Hello World"}
 
 
 @app.get("/hello/{name}")
 async def say_hello(name: str):
-    return {"message": f"Hello {name}"}
+  return {"message": f"Hello {name}"}
+
+
+app.include_router(todo_router)
