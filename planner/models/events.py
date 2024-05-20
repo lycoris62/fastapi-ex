94d4e78e921f6ef92d5300f89bf6f4dcd7db42fa
barami62 <wodbs7758@naver.com>
2024-05-20 16:01:43 +0900

    Database 연결 설정

--- planner/models/events.py
+++ planner/models/events.py
@@ -1,20 +1,41 @@
-from typing import List
+from typing import Optional, List
 
-from pydantic import BaseModel
+from sqlmodel import SQLModel, Field, Column, JSON
 
 
-class Event(BaseModel):
-  id: int
+class Event(SQLModel, table=True):
+  id: Optional[int] = Field(default=None, primary_key=True)
   title: str
   image: str
   description: str
-  tags: List[str]
   location: str
+  tags: List[str] = Field(sa_column=Column(JSON))
 
   class Config:
+    arbitrary_types_allowed = True
     json_schema_extra = {
       "example": {
         "id": 1,
+        "title": "title01",
+        "image": "https://cataas.com/cat",
+        "description": "desc01",
+        "tags": ["cat", "cute"],
+        "location": "Korea"
+      }
+    }
+
+
+class EventUpdate(SQLModel):
+  title: Optional[str]
+  image: Optional[str]
+  description: Optional[str]
+  tags: Optional[List[str]]
+  location: Optional[str]
+
+  class Config:
+    arbitrary_types_allowed = True
+    json_schema_extra = {
+      "example": {
         "title": "title01",
         "image": "https://cataas.com/cat",
         "description": "desc01",
