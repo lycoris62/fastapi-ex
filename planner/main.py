0f9c2eebf23398f4d6b5fca2099b25301f06abf4
barami62 <wodbs7758@naver.com>
2024-05-20 15:16:59 +0900

    이벤트 CRUD 추가

--- planner/main.py
+++ planner/main.py
@@ -1,11 +1,13 @@
 import uvicorn
 from fastapi import FastAPI
 
+from routes.events import event_router
 from routes.users import user_router
 
 app = FastAPI()
 
 app.include_router(user_router, prefix="/user")
+app.include_router(event_router, prefix="/event")
 
 if __name__ == "__main__":
   uvicorn.run("main.app", host="127.0.0.1", port=8000, reload=True)
