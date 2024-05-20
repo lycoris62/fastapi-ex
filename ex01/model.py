df7946a9c984801f47646065fa729b52da30ed66
barami62 <wodbs7758@naver.com>
2024-05-20 13:16:50 +0900

    투두 수정 및 삭제 API 추가

--- ex01/model.py
+++ ex01/model.py
@@ -4,3 +4,22 @@ from pydantic import BaseModel
 class Todo(BaseModel):
   id: int
   item: str
+
+  class Config:
+    json_schema_extra = {
+      "example": {
+        "id": 1,
+        "item": "Example Schema!"
+      }
+    }
+
+
+class TodoItem(BaseModel):
+  item: str
+
+  class Config:
+    json_schema_extra = {
+      "example": {
+        "item": "Read the next chapter of the book."
+      }
+    }
