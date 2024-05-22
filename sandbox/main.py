7a605e288d9052241a21d0417a5ad95502987a2e
barami62 <wodbs7758@naver.com>
2024-05-22 11:54:36 +0900

    CORS 설정

--- sandbox/main.py
+++ sandbox/main.py
@@ -1,5 +1,6 @@
 from fastapi import FastAPI, Depends
 from sqlalchemy.orm import Session
+from starlette.middleware.cors import CORSMiddleware
 
 import crud
 from api.boards.router import board_router
@@ -7,7 +8,20 @@ from api.posts.router import post_router
 from api.users.router import user_router
 from database.connection import engine, Base, get_db
 
+origins = [
+  "http://127.0.0.1:8000",
+  "http://localhost:8080",
+]
+
 app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,  # 더 자세한 정보는 여기에. https://fastapi.tiangolo.com/ko/tutorial/cors/
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 app.include_router(user_router, prefix="/users")
 app.include_router(board_router, prefix="/boards")
