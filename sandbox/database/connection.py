f0c92767ae78a5a60e37ed7e6292f722a70e9764
barami62 <wodbs7758@naver.com>
2024-05-22 11:51:13 +0900

    환경변수 넣기

--- sandbox/database/connection.py
+++ sandbox/database/connection.py
@@ -1,9 +1,16 @@
+import os
+
+from dotenv import load_dotenv
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+load_dotenv(os.path.join(BASE_DIR, ".env"))
+
 # SQLALCHEMY_DATABASE_URL = "sqlite:///./sql_app.db"
-SQLALCHEMY_DATABASE_URL = "postgresql://dev:devpw@127.0.0.1:21000/dev"
+# SQLALCHEMY_DATABASE_URL = "postgresql://dev:devpw@127.0.0.1:21000/dev"
+SQLALCHEMY_DATABASE_URL = os.environ["DB_URL"]
 
 engine = create_engine(
     SQLALCHEMY_DATABASE_URL,
